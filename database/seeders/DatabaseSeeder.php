1a35def1a268dae28f48ceb7833d277745a293e3
Almas <namazbaev2000@gmail.com>
2021-05-28 16:42:44 +0600

    Attributes Section Part 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,5 +17,6 @@ class DatabaseSeeder extends Seeder
         $this->call(AdminsTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
+        $this->call(AttributesTableSeeder::class);
     }
 }
